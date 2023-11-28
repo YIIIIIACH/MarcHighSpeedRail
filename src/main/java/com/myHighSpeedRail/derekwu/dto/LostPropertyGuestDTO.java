d23df0e9455afa614c8df6659d1b6fe3b5879d3c
YIIIIIACH <1997marc212@gmail.com>
2023-11-29 00:04:24 +0800

    修正test package(不影響spring boot的booting)
    原本test package
    的MarcHighSpeedRailApplication放在錯誤的位置，不影響運行待是要打包時會衝突。所以進行修正

--- src/main/java/com/myHighSpeedRail/derekwu/dto/LostPropertyGuestDTO.java
+++ src/main/java/com/myHighSpeedRail/derekwu/dto/LostPropertyGuestDTO.java
@@ -2,7 +2,7 @@ package com.myHighSpeedRail.derekwu.dto;
 
 import java.util.Date;
 
-public class LostPropertyDTO {
+public class LostPropertyGuestDTO {
 	public String stationName;
 	public Date findDate;
 	public String stayStation;
