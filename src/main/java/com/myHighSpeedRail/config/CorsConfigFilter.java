77efb865cf987db155638256b5f278bf0f1566c4
PeterChen7 <peter120289@gmail.com>
2023-12-01 09:37:15 +0800

    更改為可接納多前端來源
    CorsConfigFilter優化為可接納多前端來源，SessionLoginEmployee新增toString方法

--- src/main/java/com/myHighSpeedRail/config/CorsConfigFilter.java
+++ src/main/java/com/myHighSpeedRail/config/CorsConfigFilter.java
@@ -1,6 +1,8 @@
 package com.myHighSpeedRail.config;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter(filterName = "CorsFilter", urlPatterns = "/*")
@@ -23,14 +26,21 @@ public class CorsConfigFilter implements Filter {
 	@Value("${front.backstage.host}")
 	private String backStage;
 
-	private String url = front + ", " + backStage;
-
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
+		
 		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		//根據網址判斷來設定Access-Control-Allow-Origin，以達成可接納多來源的效果
+		if (front.equals(request.getHeader("Origin"))) {
+	        response.setHeader("Access-Control-Allow-Origin", front);
+	    } else if (backStage.equals(request.getHeader("Origin"))) {
+	        response.setHeader("Access-Control-Allow-Origin", backStage);
+	    }
+		
 		// addAllowedOrigin 不能設定為* 因為與 allowCredential 衝突
-		response.setHeader("Access-Control-Allow-Origin", front);
+//		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
