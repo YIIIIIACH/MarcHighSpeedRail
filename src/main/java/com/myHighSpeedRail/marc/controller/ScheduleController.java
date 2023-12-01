694f476aa09f7c93c5a6c6d71b60d2bafa5fb4d6
YIIIIIACH <1997marc212@gmail.com>
2023-12-01 23:49:22 +0800

    修改票種班次座位回傳dto複雜度

--- src/main/java/com/myHighSpeedRail/marc/controller/ScheduleController.java
+++ src/main/java/com/myHighSpeedRail/marc/controller/ScheduleController.java
@@ -225,6 +225,9 @@ public class ScheduleController {
 		
 		Integer stStSeq = rrss1.getRailRouteStopStationSequence();
 		Integer edStSeq = rrss2.getRailRouteStopStationSequence();
+		if(stStSeq >= edStSeq) {
+			return new BookingBuinessSeatResponseDto();
+		}
 		Long mask = 0L;
 		mask |= (1L << edStSeq)-1;
 		mask >>= stStSeq;
@@ -233,7 +236,7 @@ public class ScheduleController {
 		ScheduleDetail schd = schdServ.getScheduleDiscountRange(sch.getScheduleId(), bbsDto.ticketDiscountName).get(0); 
 		List<ScheduleSeatStatus> schssList =schssServ.findByScheduleSeatRange(sch, schd.getSeatRangeStart(), schd.getSeatRangeEnd()	);
 		BookingBuinessSeatResponseDto res = new BookingBuinessSeatResponseDto();
-		res.scheduleSeatStatusList= schssList;
+		res.seatList= schssList.stream().map((a)-> a.getSeat()).toList();
 		res.bookedSeatIdList = new ArrayList<Integer>();
 		for( ScheduleSeatStatus schss : schssList) {
 			if((schss.getScheduleStatus() & mask) >0) {
