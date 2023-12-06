e2b66c0f0bc4e8f9ee3fe69f8c6f74cdeb8c6515
YIIIIIACH <1997marc212@gmail.com>
2023-12-06 13:30:36 +0800

    write a test cookie controller

--- src/main/java/com/myHighSpeedRail/marc/controller/FirstController.java
+++ src/main/java/com/myHighSpeedRail/marc/controller/FirstController.java
@@ -1,7 +1,14 @@
 package com.myHighSpeedRail.marc.controller;
 
+import java.util.Enumeration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
@@ -9,6 +16,17 @@ public class FirstController {
 	@GetMapping("/")
 	public String home() {
 		return "test.html";
+	}
+	
+	@GetMapping("/getClientIP")
+	public @ResponseBody ResponseEntity<String> getClientIP(HttpServletRequest req){
+//		Enumeration<String> hNames = req.getHeaderNames();
+////		req.getHeader("connection");
+//		do {
+//			System.out.println(req.getHeader(hNames.nextElement()));
+//		}while( hNames.hasMoreElements());
+		System.out.println( req.getHeader("host"));
+		return new ResponseEntity<String>("get header names",HttpStatus.OK);
 	}
 
 }
