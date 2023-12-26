0d277fd1bbd0153401d90c0699a80cf3779a355a
wowowork12345 <wowowork12345>
2023-12-26 18:49:45 +0800

    遺失物完成

--- src/main/java/com/myHighSpeedRail/derekwu/controller/LostPropertyPhotoController.java
+++ src/main/java/com/myHighSpeedRail/derekwu/controller/LostPropertyPhotoController.java
@@ -1,11 +1,16 @@
 package com.myHighSpeedRail.derekwu.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.myHighSpeedRail.derekwu.model.LostProperty;
 import com.myHighSpeedRail.derekwu.repository.LostPropertyRepository;
@@ -24,5 +29,19 @@ public class LostPropertyPhotoController {
 			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(photo);
 			
 		}
+	//上傳遺失物圖片(員工)
+		@PostMapping("/LostProperty/backend/postImage/{id}")
+		public ResponseEntity<?> postImage(@PathVariable("id") Integer id,MultipartFile lostPhoto) throws IOException{
+			LostProperty lostProperty = lpRepo.findByLostPropertyId(id);
+			if (lostPhoto != null) {
+	            byte[] lostPhotoBytes = lostPhoto.getBytes();
+	            lostProperty.setLostPhoto(lostPhotoBytes);
+	        }
+	        LostProperty savedLostProperty = lpRepo.save(lostProperty);
+	        return new ResponseEntity<>(savedLostProperty, HttpStatus.CREATED);
+
+
+		}
+
 
 }
